4011ceaa6e2f620b1d2148d5d761e2484a012099
Danilo Machuca <danilo.machuca.dev@gmail.com>
2025-08-26 22:18:55 -0300

    reducer do carrinho criado

---
+++ src/store/reducers/cart.ts
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import type { Game } from '../../pages/Home'
+
+type CartState = {
+  items: Game[]
+}
+
+const initialState: CartState = {
+  items: []
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<Game>) => {
+      state.items.push(action.payload)
+    }
+  }
+})
+
+export const { add } = cartSlice.actions
+export default cartSlice.reducer
